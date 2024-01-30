de266a9b7f0e0f2c6a095027e6836042f974b239
Ke Wei || Luna <42366824+KWLuna@users.noreply.github.com>
2024-01-30 09:15:29 +0800

    Added notes in Update TodoRepository.java

--- 02.Spring-Boot-Web-Application-V2/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoRepository.java
+++ 02.Spring-Boot-Web-Application-V2/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoRepository.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// Allow Action to be performed on entities.
+// Repo is Interface. Naming Convention: Entity+Repository
+// Must extend JpaRepository
+// <What bean its managing, Type of ID field>
 public interface TodoRepository extends JpaRepository<Todo, Integer>{
 	public List<Todo> findByUsername(String username);
 }
